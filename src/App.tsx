ad2f41dc843306391672c926a1893f58d5389728
GAM3RG33K <GAM3RG33K@users.noreply.github.com>
2024-09-04 17:43:53 +0000

    deploy: dafc28e8a0a4bb8d42d5310ee0458e2dcdd7749b

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import FreelanceEarningsCalculator from './FreelanceEarningsCalculator'
+
+function App() {
+  return (
+    <div className="App">
+      <FreelanceEarningsCalculator />
+    </div>
+  )
+}
+
+export default App
